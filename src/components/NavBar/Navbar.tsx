cdf1307f6792772f2901aef4a979415e51a6eb07
rscalderon <127636148+rscalderon@users.noreply.github.com>
2024-04-08 12:46:16 -0400

    Moved Navbar component and stylesheet into own directory with index.js export file

---
+++ src/components/NavBar/Navbar.tsx
@@ -0,0 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+
+import './NavBar.scss';
+
+// Renders Navbar component
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <header className='navbar'>
+        <p className='navbar-title' onClick={() => navigate('/')}>
+          Zealthy Help Desk
+        </p>
+        <nav id='navbar-right'>
+          <p className='navbar-element' onClick={() => navigate('/login')}>
+            Login
+          </p>
+          <p className='navbar-element' onClick={() => navigate('/dashboard')}>
+            Dashboard
+          </p>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
