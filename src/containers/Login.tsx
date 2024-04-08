776a3b7b5cf2115f11cb2058b961909ef352ec9b
rscalderon <127636148+rscalderon@users.noreply.github.com>
2024-04-08 12:48:13 -0400

    Added home styling for Login components

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -7,38 +7,40 @@ Note: For the sake of time, I built the UI but did not implement the auth busine
 */
 
 import { useNavigate } from 'react-router-dom';
+import './Home.scss';
 
 function Login() {
   const navigate = useNavigate();
   return (
     <div className='form-container'>
       <div className='login-container'>
-        <h2>Admin Log In</h2>
+        <h2>Admin Login</h2>
         <form
-          className='login-form'
+          className='form'
           name='login'
           onSubmit={() => navigate('/dashboard')}
         >
-          <label htmlFor='email'>Email</label>
-          <input
-            type='email'
-            id='email'
-            name='email'
-            placeholder='Enter your email here'
-            autoFocus
-            required
-          ></input>
-          <br />
-          <label htmlFor='password'>Password</label>
-          <input
-            type='text'
-            id='password'
-            name='password'
-            placeholder='Enter your password here'
-            required
-          ></input>
-          <br />
-          <br />
+          <div className='input-and-label'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              placeholder='Enter your email here'
+              autoFocus
+              required
+            ></input>
+          </div>
+          <div className='input-and-label'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='text'
+              id='password'
+              name='password'
+              placeholder='Enter your password here'
+              required
+            ></input>
+          </div>
           <button
             className='login-form-button'
             // Note: With more time, this would be replaced with a robust authentication and authorization feature
@@ -47,7 +49,6 @@ function Login() {
           </button>
         </form>
         <div className='login-form-links'>
-          <br />
           <button onClick={() => console.log('Trigger reset password feature')}>
             Forgot your password?
           </button>
