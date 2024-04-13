b2b53a8578149588a26e07ffeae51c577d84131c
rscalderon <127636148+rscalderon@users.noreply.github.com>
2024-04-13 16:03:36 -0400

    Created services file with utils extracted from components

---
+++ src/utils/services.ts
@@ -0,0 +1,65 @@
+/**
+ * ************************************
+ *
+ * @module  getTickets
+ * @description Retrieves tickets from database
+ * and returns a promise that resolves to an object
+ * of ticket information
+ *
+ * ************************************
+ */
+
+export const getTickets = async () => {
+  return fetch('/api/tickets')
+    .then((data) => data.json())
+    .catch((e) =>
+      console.error(
+        `Would normally log to monitoring service: ${{
+          error: `Error retrieving helpdesk tickets: ${e}`,
+        }}`
+      )
+    );
+};
+
+/**
+ * ************************************
+ *
+ * @module  postTicket
+ * @param  {string} name Author name
+ * @param  {string} email Author email
+ * @param  {string} description Ticket description
+ * @description Adds ticket to database and "sends notification email".
+ * Returns a promise
+ *
+ * ************************************
+ */
+
+export const postTicket = (
+  name: string,
+  email: string,
+  description: string
+) => {
+  return fetch('/api/tickets', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      description: description,
+    }),
+  })
+    .then(() =>
+      console.log(
+        `Would normally send email here to administrator with copy to ticket author with: \n \n Title: 'New Helpdesk Ticket' \n \n Body: New helpdesk ticket from ${name} (${email}): \n \n ${description} \n \n You can respond directly to this email address or log on to the zealthy helpdesk portal to respond.`
+      )
+    )
+    .catch((e) =>
+      console.error(
+        `Would normally log to monitoring service: ${{
+          error: `Error creating helpdesk ticket: ${e}`,
+        }}`
+      )
+    );
+};
